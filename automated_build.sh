3319da1ab79422bd06ab7051678a4c4a3055c781
David Litvak Bruno <david.litvakb@gmail.com>
2016-06-20 10:06:49 -0300

    testing script

--- automated_build.sh
+++ automated_build.sh
@@ -6,17 +6,23 @@ cd /tmp
 git clone git@github.com:BreadNet/breadnet.github.io.git build
 
 # Trigger Middleman Rebuild
+echo `pwd`
 cd $CWD
+echo `pwd`
 bundle exec middleman contentful --rebuild
 
 # Push Newly built Repo
+
+echo "$CWD/build"
 cp -r $CWD/build /tmp/build
 
 cd /tmp/build
+
+echo "$(ls -l)"
 
 git config --global user.email "david.litvakb@gmail.com"
 git config --global user.name "David Litvak Bruno"
 
 git add .
 git commit -m "Automated Rebuild"
-git push -f origin master
+#git push -f origin master
